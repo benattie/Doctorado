9c42773164c8ad20dd627ae8071d414711ba9c12
benattie <benatti@ifir-conicet.gov.ar>
2014-02-19 16:13:50 -0300

    Completado del commit anterior. Me quedo sin agregar el spr2rsts.py modificado

--- spr2rsts-2.1_spr2txt-1.0/spr2rsts.py
+++ spr2rsts-2.1_spr2txt-1.0/spr2rsts.py
@@ -5,7 +5,11 @@ import sys
 import numpy as np
 import math
 import subprocess as sub
+import re
 #########################################################################
+#print str(sys.argv)
+#for i in range(0,8):
+#    print "Argv " + str(i) + "is: " + str(sys.argv[i])
 
 name = str(sys.argv[1]) # nombre del archivo de entrada (marfile)
 separator = str(sys.argv[2]) # separador del archivo spr ("  ")
@@ -41,9 +45,26 @@ comment = "#"
 f_rst = open(str(sys.argv[4]), 'w') #rsts_file
 n = int(sys.argv[5]) #numrings
 
+para_data = open("para_fit2d.dat", 'r')
+buf_str = para_data.readlines()[22:]
+fit_cmd = ''
+for line in buf_str:
+    line = re.split(" +", line) #convierto la linea en un array de strings
+
+    aux1 = float(line[4])
+    aux1 = math.atan(aux1 * 100e-6 / 1081e-3) * 180. / math.pi
+
+    aux2 = float(line[5])
+    aux2 = math.atan(aux2 * 100e-6 / 1081e-3) * 180. / math.pi
+#    print "%.3f %.3f\n" % (aux1, aux2)
+    fit_cmd = fit_cmd + 'FIT_SINGLE_PEAK %.3f %.3f' % (aux1, aux2) + '\n'
+#    print fit_cmd
+para_data.close()
+
 #genero los difractogramas que quiero procesar automaticamente
 for i in range(0, nrow, res): #recorro las filas del archivo de res en res
-    name_dat = 'dif' + '{:0>3}'.format(str(i)) + '.dat' #genero el archivo de Inv vs 2theta
+    buf = str(sys.argv[6]) #sys.argv[6] = difs_file
+    name_dat = buf + 'dif' + '{:0>3}'.format(str(i)) + '.dat' #genero el archivo de Inv vs 2theta
     f_dat = open(name_dat, 'w')
     f_dat.write('!2theta\tInt\n')
     for j in range(0, ncol):
@@ -58,14 +79,15 @@ for i in range(0, nrow, res): #recorro las filas del archivo de res en res
     name_cmd = 'fit_winplotr_' + '{:0>3}'.format(str(i)) + '.cmd' #genero el archivo con los comandos de winplotr
     f_cmd = open(name_cmd, 'w')
     f_cmd.write('FILE ' + name_dat + ' 1\n')
-    f_cmd.write('FIT_SINGLE_PEAK 3.392 3.578\nFIT_SINGLE_PEAK 3.948 4.108\nFIT_SINGLE_PEAK 5.618 5.830\nFIT_SINGLE_PEAK 6.625 6.784\nFIT_SINGLE_PEAK 6.916 7.076\nFIT_SINGLE_PEAK 8.003 8.215\nFIT_SINGLE_PEAK 8.745 8.957\n')#esto lo podria armar con los datos del para_fit2d.dat
+    #f_cmd.write('FIT_SINGLE_PEAK 3.177 3.705\nFIT_SINGLE_PEAK 3.863 4.312\nFIT_SINGLE_PEAK 5.548 5.863\nFIT_SINGLE_PEAK 6.518 6.857\nFIT_SINGLE_PEAK 6.857 7.145\nFIT_SINGLE_PEAK 7.900 8.186\nFIT_SINGLE_PEAK 8.653 8.937\n')#esto lo podria armar con los datos del para_fit2d.dat
+    f_cmd.write(fit_cmd)
     f_cmd.close()
     sub.call('winplotr ' + name_cmd) #llamo al archivo de winplotr que acabo de generar
-    sub.call('rm *.OUT *.REF *.XRF *.cmd')     #limpio el directorio de todos los archivos innecesarios (me quedo solamente con los archivos RST y los new)
+
 
     #paso los datos del rst al rsts
-    name_rst = 'DIF' + '{:0>3}'.format(str(i)) + '_PF.RST' #abro el archivo que va a contener todos los rst
-    f_rst.write(comment + name_rst + ' ' + str(sys.argv[6]) + '\n') #sys.argv[6] == filename1
+    name_rst = buf.upper() + 'DIF' + '{:0>3}'.format(str(i)) + '_PF.RST' #abro el archivo que va a contener todos los rst
+    f_rst.write(comment + name_rst + ' ' + str(sys.argv[7]) + '\n') #sys.argv[7] == filename1
     f_rst.write(comment + '2theta(deg.)\tsig_2th\tIntensity\tsig_int\tFWHM\tFWHM_sig\tETA\tETA_sig\n')
     f_rst.flush() #vacio el buffer antes de seguir escribiendo
     rst_handler = open(name_rst, 'r') #abro el archvio rst del que voy a sacar los datos
